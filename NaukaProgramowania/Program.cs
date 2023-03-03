4b9fea656a2ff9ddd7f5067c6ec79940bc79dd68
Wojciech <nowwak.wojtek@gmail.com>
2023-03-03 18:24:22 +0100

    Initial comit

---
+++ NaukaProgramowania/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaProgramowania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
