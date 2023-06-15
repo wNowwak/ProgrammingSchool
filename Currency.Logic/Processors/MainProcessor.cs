573117461d52df5676af2b08e5e2cfb4f9fd5830
Wojciech <nowwak.wojtek@gmail.com>
2023-06-15 20:12:18 +0200

    LINQ

--- Currency.Logic/Processors/MainProcessor.cs
+++ Currency.Logic/Processors/MainProcessor.cs
@@ -1,5 +1,7 @@
 ﻿using Currency.Logic.Extensions;
 using Currency.Logic.Interfaces;
+using Currency.Models;
+using System.Globalization;
 
 namespace Currency.Logic.Processors;
 
@@ -14,29 +16,55 @@ public class MainProcessor
 
     public void Process()
     {
-        Console.WriteLine("Pobierz aktualny kurs waluty - 1" +
-            "Pobierz kilka kursów waluty - 2" +
-            "Pobierz kurs w konkretnym dniu - 3");
+        Console.WriteLine("Pobierz aktualny kurs waluty - 1 \n" +
+            "Pobierz kilka kursów waluty - 2 \n" +
+            "Pobierz kurs w konkretnym dniu - 3 \n" +
+            "Średnia ostatnich 10 notowań - 4 \n" +
+            "Najwyższe notowanie z ostatnich 10 - 5 \n");
         var option = Console.ReadLine();
         Console.WriteLine("Jaką walutę pobrać?");
-        var currency = Console.ReadLine()!;
+        var currencyCode = Console.ReadLine()!;
+        var currency = new CurrencyDto();
         switch (option)
         {
             case "1":
-                _currencyWebClient.GetCurrencyByCode(currency).Display();
+                currency = _currencyWebClient.GetCurrencyByCode(currencyCode);
+                currency!.Display();
                 break;
             case "2":
                 Console.WriteLine("Ile kursów pobrać?");
                 var count = int.Parse(Console.ReadLine()!);
-                _currencyWebClient.GetLastCurrencyRateByCode(currency, count).Display();
+                currency = _currencyWebClient.GetLastCurrencyRateByCode(currencyCode, count);
+                currency!.Display();
                 break;
             case "3":
-                var data = 
-                _currencyWebClient.GetCurrencyRateInSpecificDate(currency);
+                Console.WriteLine("Podaj datę w formacie DD-MM-YYYY");
+                var data = Console.ReadLine();
+                currency = _currencyWebClient.GetCurrencyRateInSpecificDate(currencyCode, DateTime.ParseExact(data!, "dd-MM-yyyy",
+                    CultureInfo.InvariantCulture));
+                currency!.Display();
+                break;
+            case "4": //Piotrek
+                //Średnia ostatnich 10 notowań
+                currency = _currencyWebClient.GetLastCurrencyRateByCode(currencyCode, 10);
+                //var sumanotowan = 0.0m;
+                //foreach (var rates in currency.Rates)
+                //{
+                //    sumanotowan += rates.Mid;
+                //}
+                //var srednia = sumanotowan / currency.Rates.Count;
+                var srednia2 = currency!.Rates.Average(rate => rate.Mid);
+                break;
+            case "5": //Marcin
+                currency = _currencyWebClient.GetLastCurrencyRateByCode(currencyCode, 10);
+                var currencyHighestRate = currency!.Rates.Select(_ => _.Mid).ToList();
+                decimal max = currencyHighestRate.Max();
                 break;
             default:
                 break;
+
         }
+
         Console.ReadLine();
     }
 }
